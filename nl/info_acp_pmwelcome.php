dc54eb09aa87481e9e4067b660a6ec9976408055
David <neurofibromatose@live.nl>
2015-09-17 21:04:11 +0200

    eerste vertaling

--- nl/info_acp_pmwelcome.php
+++ nl/info_acp_pmwelcome.php
@@ -2,9 +2,10 @@
 
 /**
 *
-* @package PM Welcome [English]
+* @package PM Welcome [Dutch]
 * @copyright BB3.MOBi (c) 2015 Anvar http://apwa.ru
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* Dutch translation by Dutch Translators (https://github.com/dutch-translators)
 *
 */
 if (!defined('IN_PHPBB'))
@@ -18,12 +19,12 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_PMWELCOME'					=> 'Welcome message',
-	'ACP_PMWELCOME_EXPLAIN'			=> 'You can specify the text of the personal message that will be sent to the user upon registration.',
-	'ACP_PMWELCOME_SETTINGS'		=> 'Settings welcome message.',
-	'ACP_PMWELCOME_USER'			=> 'Sender',
-	'ACP_PMWELCOME_USER_EXPLAIN'	=> 'id User Forum, on behalf of which will be sent to a private message.',
-	'ACP_PMWELCOME_SUBJECT'			=> 'Post subject',
-	'ACP_PMWELCOME_TEXT'			=> 'Text of the greeting message',
-	'ACP_PMWELCOME_TEXT_EXPLAIN'	=> 'You can use bbcode and smilies, and the token {USERNAME} to replace the name of the user who receive a private message.',
+	'ACP_PMWELCOME'					=> 'Welkomstbericht',
+	'ACP_PMWELCOME_EXPLAIN'			=> 'Je kan hier een persoonlijkbericht samenstellen, die wordt verstuurd als er een nieuwe registratie is.',
+	'ACP_PMWELCOME_SETTINGS'		=> 'Instellingen welkomstbericht.',
+	'ACP_PMWELCOME_USER'			=> 'Afzender',
+	'ACP_PMWELCOME_USER_EXPLAIN'	=> 'Gebruikers-ID, van de gebruiker namens wie het welkomstbericht wordt verstuurd.',
+	'ACP_PMWELCOME_SUBJECT'			=> 'Onderwerp bericht',
+	'ACP_PMWELCOME_TEXT'			=> 'Tekst van het welkomstbericht',
+	'ACP_PMWELCOME_TEXT_EXPLAIN'	=> 'Je kan BBcodes en samilies gebruiken en de variabel {USERNAME}, die automatische wordt vervangen door de naam van de nieuwe geregistreerdegebruiker.',
 ));
